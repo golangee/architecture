6b439a15b345e9dfc8d363299b62155005274c70
Lars Martens <lars.martens@worldiety.de>
2021-08-09 14:34:28 +0200

    Attempt recursive validation

--- arc/model/artifact.go
+++ arc/model/artifact.go
@@ -1,5 +1,7 @@
 package model
 
+import "fmt"
+
 type Artifacts struct {
 	// Values contains all the value objects.
 	Values []NamedWithFields `tadl:"value"`
@@ -9,6 +11,16 @@ type Artifacts struct {
 	// same name. The will get a unique Id when generated.
 	Entities   []NamedWithFields `tadl:"entity"`
 	Aggregates []Aggregate       `tadl:"aggregate"`
+}
+
+func (a *Artifacts) Validate(project *Project) error {
+	for _, aggregate := range a.Aggregates {
+		if err := aggregate.Validate(project); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Aggregate is a combination of several value objects and entities.
@@ -22,12 +34,35 @@ type Aggregate struct {
 	Methods map[string]Method `tadl:"methods"`
 }
 
+func (a *Aggregate) Validate(project *Project) error {
+	for methodName, method := range a.Methods {
+		foundAny := false
+
+		for _, story := range project.Stories {
+			if method.Story == story.Id {
+				foundAny = true
+				break
+			}
+		}
+
+		if !foundAny {
+			// TODO Proper error types
+			return fmt.Errorf("aggregate '%s' method '%s' references story with ID %d, but such a story does not exist", a.Name, methodName, method.Story)
+		}
+	}
+
+	return nil
+}
+
 // Method has a description and input and output parameters.
 // Both parameters are maps from name to type.
 type Method struct {
-	Description string            `tadl:",inner"`
-	Params      map[string]string `tadl:",inner"`
-	Returns     map[string]string `tadl:"ret"`
+	Description string `tadl:",inner"`
+	// Because is a reference to the ID of a user story that explains why
+	// this method should exist.
+	Story   int               `tadl:"story,attr"`
+	Params  map[string]string `tadl:",inner"`
+	Returns map[string]string `tadl:"ret"`
 }
 
 // NamedWithFields can be a value object or an entity.
--- arc/model/read_test.go
+++ arc/model/read_test.go
@@ -10,23 +10,27 @@ import (
 func TestRead(t *testing.T) {
 	proj, err := ParseWorkspace("/Users/larsmartens/repos/golangee/architecture/testdata/supportiety")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("parser error:", err)
+	}
+
+	if err := proj.Validate(proj); err != nil {
+		t.Fatal("validation failed:", err)
 	}
 
 	indented, err := json.MarshalIndent(proj, "", "    ")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("could not create marshalled json:", err)
 	}
 
 	f, err := os.Create("test_out.json")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("failed to create output file:", err)
 	}
 
 	defer f.Close()
 
 	_, err = f.Write(indented)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("failed to write to file", err)
 	}
 }
